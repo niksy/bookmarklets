511e2d71083a02571ced14d987f2c585d5c9126f
Ivan Nikolić <niksy5@gmail.com>
2014-01-25 13:12:18 +0100

    Add Pinboard "Read Later" saving
    Ref. https://pinboard.in/howto/#saving

---
+++ src/pinboard-read-later.original.js
@@ -0,0 +1,1 @@
+javascript:q=location.href;p=document.title;void(t=open('https://pinboard.in/add?later=yes&noui=yes&jump=close&url='+encodeURIComponent(q)+'&title='+encodeURIComponent(p),'Pinboard','toolbar=no,width=100,height=100'));t.blur();
