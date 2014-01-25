762a64742d1e8bf021f58e614a5be5410a1b433e
Ivan Nikolić <niksy5@gmail.com>
2014-01-25 12:06:28 +0100

    Add Twitter share
    Ref. https://github.com/filamentgroup/SocialCount/blob/master/examples/index.html#L133

---
+++ src/twitter-share.js
@@ -0,0 +1,5 @@
+window.open(
+	'https://twitter.com/intent/tweet?text=' + window.location.href,
+	'socialNetworkShare',
+	'width=600,height=600,top=100,left=100,menubar=no,scrollbars=no,status=no,toolbar=no'
+);
