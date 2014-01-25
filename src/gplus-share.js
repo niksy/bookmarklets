8a24c894f4f734332dc59c46f8aad8301a1cebd8
Ivan Nikolić <niksy5@gmail.com>
2014-01-25 12:06:48 +0100

    Add Google+ share
    Ref. https://github.com/filamentgroup/SocialCount/blob/master/examples/index.html#L134

---
+++ src/gplus-share.js
@@ -0,0 +1,5 @@
+window.open(
+	'https://plus.google.com/share?url=' + window.location.href,
+	'socialNetworkShare',
+	'width=600,height=600,top=100,left=100,menubar=no,scrollbars=no,status=no,toolbar=no'
+);
